f2b6cc865ebd03362c0b6c84b344810c16e67aca
Ashcon Mohseninia <ashconm@outlook.com>
2025-01-20 21:24:23 +0000

    Test implementation of async read for ADC

--- hal/src/peripherals/adc/d5x/async_api.rs
+++ hal/src/peripherals/adc/d5x/async_api.rs
@@ -2,24 +2,22 @@ use core::marker::PhantomData;
 
 use crate::async_hal::interrupts::Handler;
 
+use embassy_sync::waitqueue::AtomicWaker;
 
-pub(super) mod waker {
-    use embassy_sync::waitqueue::AtomicWaker;
+#[allow(clippy::declare_interior_mutable_const)]
+const NEW_WAKER: AtomicWaker = AtomicWaker::new();
+pub static ADC_WAKERS: [AtomicWaker; 2] = [NEW_WAKER; 2];
 
-    #[allow(clippy::declare_interior_mutable_const)]
-    const NEW_WAKER: AtomicWaker = AtomicWaker::new();
-    pub static ADC_WAKERS: [AtomicWaker; super::super::NUM_ADC] = [NEW_WAKER; super::super::NUM_ADC];
-}
 
 /// Interrupt handler for the ADC peripheral.
-pub struct InterruptHandler<A: super::Adc> {
+pub struct InterruptHandler<A: crate::adc::Adc> {
     _private: (),
     _adc: PhantomData<A>
 }
 
-impl<A: super::Adc> crate::typelevel::Sealed for InterruptHandler<A>{}
+impl<A: crate::adc::Adc> crate::typelevel::Sealed for InterruptHandler<A>{}
 
-impl<A: super::Adc> Handler<A::Interrupt> for InterruptHandler<A> {
+impl<A: crate::adc::Adc> Handler<A::Interrupt> for InterruptHandler<A> {
     unsafe fn on_interrupt() {
         let mut peripherals = unsafe { crate::pac::Peripherals::steal() };
         let adc = A::reg_block(&mut peripherals);
