5cff7ed43a16285f2fd33739fe1f343ebb400948
Justin Beaurivage <code@beaurivage.io>
2025-02-04 23:02:40 -0500

    Minimal working ADC example for feather_m0

--- boards/feather_m0/examples/adc.rs
+++ boards/feather_m0/examples/adc.rs
@@ -1,13 +1,10 @@
 #![no_std]
 #![no_main]
 
-// #[cfg(not(feature = "use_semihosting"))]
-// use panic_halt as _;
-// #[cfg(feature = "use_semihosting")]
-// use panic_semihosting as _;
-
-use defmt_rtt as _;
-use panic_probe as _;
+#[cfg(not(feature = "use_semihosting"))]
+use panic_halt as _;
+#[cfg(feature = "use_semihosting")]
+use panic_semihosting as _;
 
 use cortex_m_semihosting::hprintln;
 
@@ -16,10 +13,14 @@ use bsp::pac;
 use feather_m0 as bsp;
 
 use bsp::entry;
-use hal::adc::{Accumulation, Adc, Config, Prescaler, Resolution};
+use hal::adc::{Accumulation, Adc, Adc0, Config, Prescaler, Resolution};
 use hal::clock::GenericClockController;
 use hal::prelude::*;
 use pac::{CorePeripherals, Peripherals};
+
+atsamd_hal::bind_interrupts!(struct Irqs {
+    ADC => atsamd_hal::adc::InterruptHandler<Adc0>;
+});
 
 #[entry]
 fn main() -> ! {
@@ -39,16 +40,19 @@ fn main() -> ! {
 
     let adc_config = Config::new()
         .clock_cycles_per_sample(5)
-        .clock_divider(Prescaler::Div4)
+        .clock_divider(Prescaler::Div8)
         .sample_resolution(Resolution::_12bit)
         .accumulation_method(Accumulation::Single);
 
-    let mut adc = Adc::new(peripherals.adc, adc_config, &mut peripherals.pm, &adc_clock).unwrap();
-    let mut a0: bsp::A0 = pins.a0.into();
+    let mut adc = Adc::new(peripherals.adc, adc_config, &mut peripherals.pm, &adc_clock)
+        .unwrap()
+        .into_future(Irqs);
+    let mut a0 = pins.a0.into_alternate();
 
     loop {
-        let data = adc.read_blocking(&mut a0);
-        defmt::info!("{}", data);
+        let mut buf = [0; 16];
+        let data = adc.read_buffer_blocking(&mut a0, &mut buf);
+        hprintln!("buf: {}", buf);
         delay.delay_ms(1000u16);
     }
 }
